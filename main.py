1c10fe7c69d07920ed9b83618fb18fd98927774a
Ajay Prajapati <ajay259prajapati@gmail.com>
2026-05-09 20:21:43 +0530

    Implemented new features and improved code flow !

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import json
 import os 
 import sys
 
+os.system("cls")
+
 if os.path.exists("data"):
     pass
 else:
@@ -17,60 +19,96 @@ except (FileNotFoundError, json.JSONDecodeError):
         json.dump(empty,f)
     with open("data/data.json", "r") as f:
         data = json.load(f)
+
+
+def load():
+    with open("data/data.json", "r") as f :
+        data = json.load(f)
+def save():
+    with open("data/data.json", "w") as f :
+        json.dump(data, f)
         
+
+
 
 def add_task(name):
     task_no = len(data) + 1 
     data[task_no] = {"name" : name , "task_done" : False}
-    print(f"Task no.{task_no} Added to list! ")
-    with open("data/data.json", "w")  as  f:
-        json.dump(data, f)
-
+    save()
+    print(Fore.GREEN + f"Task no.{task_no} Added to list! ")
 
 
 def update_task(task_number):
-    data[f"{task_number}"]["task_done"] = True
-    print(f"Task no.{task_number} updated to Done!")
-    with open("data/data.json", "w")  as  f:
-        json.dump(data, f)
-
+    key = str(task_number)
+    data[key]["task_done"] = True
+    save()
+    print(f"Task no.{task_number} updated to Done!\n")
 
     
 def get_all_task():
-    with open("data/data.json", "r") as f:
-        data = json.load(f)          
+    load()
     print("\n" +"TODO-LIST".center(50, '-')) 
     topline = "Task No.  " + "Task Name " + " "*25 + "Done "
     print(topline)
     print("-"*50)
     for i in range(1, len(data)+1) :
-        key = f"{i}"
+        key = str(i)
         name = data[key]["name"]
         done = data[key]["task_done"]
         line = key.center(10) + name.ljust(35) + str(done)
         print(line)
-    print("-"*50 + "\n")
+    print("-"*50 )
+    
     
 def get_specific_task(task_no):
-    with open("data/data.json", "r") as f:
-        data = json.load(f)
-    key = f"{task_no}"
+    load()
+    key = str(task_no)
     name = data[key]["name"]
     done  = data[key]["task_done"]
     print("\n" + "="*30)
-    print(f" ⁕ Task No. : {task_no}" + f"\n ⁕ Task Name : {name}" + f"\n ⁕ Task Done : {done}")
+    print(f" ⁕ Task No.  : {task_no}" + f"\n ⁕ Task Name : {name}" + f"\n ⁕ Task Done : {done}")
     print("="*30 + "\n")
+
+    
+def resort():
+    values = list(data.values())
+    data.clear()
+    for i,task in enumerate(values, 1) :
+        data[str(i)] = task
+    save()
+    
+    
+def remove_task(task_no):
+    load()
+    removed_task = data.pop(str(task_no))
+    print(f"Task '{removed_task["name"]}' Removed !\n")
+    resort()
+    save()
+    
+def edit_task(task_no, new_name, task_status = False):
+    load()
+    data[str(task_no)]["name"] = new_name
+    if task_status:
+        data[str(task_no)]["task_done"] = not data[str(task_no)]["task_done"]
+    save()
+    print("Task Edited Successfully >>> ")
+    get_specific_task(task_no)
     
 
+menu = "\n>>> Menu : \n1. Add Task \n2. Update Task Status \n3. List all Task \n4. List one Task \n5. Remove Task \n6. Edit Task\n"
+print(menu)
+
 while True :
-    print(">>> Menu: \n1. Add Task \n2. Update Task \n3. List all Task \n4. List one Task ")
-    cmd = input("Enter cmd no. from  menu ('Q/q' for quit) --> ")
+    cmd = input("\nEnter cmd no. from menu :  \n> 'Q/q' for quit\n> 'M/m' for menu \n---------> ")
     
     try :
         cmd_no = int(cmd)
     except ValueError as e :
         if cmd.lower() == "q" :
             sys.exit()
+        elif cmd.lower() == "m" :
+            print(menu)
+            continue
         else:
             print("Invalid Input!")
             continue
@@ -96,16 +134,56 @@ while True :
             except ValueError as e :
                 print("Invalid Input !")
                 continue
-            get_specific_task(task_no)
+            
+            try :
+                get_specific_task(task_no)
+            except KeyError as e :
+                print("Invalid Input (Task not Exist!)")
+                continue
+                
+        
+        elif cmd_no == 5 :
+            try :
+                task_no = int(input("Enter Task no. : "))
+            except ValueError as e :
+                print("Invalid Input !")
+                continue
+                
+            try :
+                remove_task(task_no)
+            except KeyError as e :
+                print("Invalid Input (Task not Exist!)")
+                continue    
+                
+        
+        elif cmd_no == 6 :
+            try :
+                task_no = int(input("Enter Task no. : "))
+            except ValueError as e :
+                print("Invalid Input !")
+                continue
+            
+            try :
+                get_specific_task(task_no)
+            except KeyError as e :
+                print("Invalid Input (Task not Exist!)")
+                continue
+            else:    
+                print("You want to edit above Task > ")
+                new_name = input("\nEnter new Task : ")
+                task_status = input("\nWant to edit task Status (y/n): ")
+                if task_status.lower() == "y" :
+                    edit_task(task_no, new_name, True)
+                else :
+                    edit_task(task_no, new_name)
+                
             
         else:
             print("Invalid Input! (cmd not available))")
             continue
 
 
-with open("data/data.json", "w")  as  f:
-    json.dump(data, f)
-
+save()
     
      
     
